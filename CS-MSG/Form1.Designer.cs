e764d757ecef6f3336c957fcb458f645e7d32fd3
shuaizhuaidym <shuaizhuaidym@sina.com>
2019-01-15 21:01:57 +0800

    first commit

---
+++ CS-MSG/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace CS_MSG {
+    partial class FrmStart {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.labelCopyRight = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.workerSSO = new System.ComponentModel.BackgroundWorker();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelCopyRight
+            // 
+            this.labelCopyRight.AutoSize = true;
+            this.labelCopyRight.Location = new System.Drawing.Point(444, 369);
+            this.labelCopyRight.Name = "labelCopyRight";
+            this.labelCopyRight.Size = new System.Drawing.Size(65, 12);
+            this.labelCopyRight.TabIndex = 0;
+            this.labelCopyRight.Text = "copy right";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 339);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(696, 15);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // workerSSO
+            // 
+            this.workerSSO.WorkerReportsProgress = true;
+            this.workerSSO.WorkerSupportsCancellation = true;
+            this.workerSSO.DoWork += new System.ComponentModel.DoWorkEventHandler(this.workerSSO_DoWork);
+            this.workerSSO.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.workerSSO_ProgressChanged);
+            this.workerSSO.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCancel.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Image = global::CS_MSG.Properties.Resources.img_close;
+            this.btnCancel.Location = new System.Drawing.Point(694, 2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(24, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = " ";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // FrmStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CS_MSG.Properties.Resources.timg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(720, 401);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelCopyRight);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmStart";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loading";
+            this.Shown += new System.EventHandler(this.FrmStart_Shown);
+            this.DoubleClick += new System.EventHandler(this.FrmStart_DoubleClick);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FrmStart_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCopyRight;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker workerSSO;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ CS-MSG/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace CS_MSG {
+    public partial class FrmStart : Form {
+        private ContextMenuStrip contextMenu;
+        
+        /**
+         * 构造函数
+         */
+        public FrmStart() {
+            InitializeComponent();
+        }
+
+        /**
+         *注册COM组件 
+         */
+        private bool registerCOM() {
+            Type myType = Type.GetTypeFromProgID("IMyLib.MyClass");
+            object obj = Activator.CreateInstance(myType);
+            object[] args = new object[2];
+            args[0] = "Hello";
+            args[1] = 3;
+            //myType.InvokeMember("MyMethod", BindingFlags.InvokeMethod, obj, args);
+
+            //In .Net 4 something like this
+
+            //Type myType = Type.GetTypeFromProgID("IMyLib.MyClass");
+            //dynamic obj = Activator.CreateInstance(myType);
+            //obj.MyMethod("Hello", 3);
+            return true;
+        }
+
+        /**
+         *窗体显示出来后，开启后台进程调用PNXClient组件点点登录
+         */
+        private void FrmStart_Shown(object sender, EventArgs e) {
+            
+            this.labelCopyRight.Text = "Copy right@长春吉大正元信息技术股份有限公司";
+            this.labelCopyRight.BackColor = System.Drawing.Color.Transparent;
+            this.labelCopyRight.ForeColor = System.Drawing.Color.Transparent;
+            //TODO 调用PNXClient获取Token做T+A业务                      
+            this.workerSSO.RunWorkerAsync();
+        }
+
+        /**
+         * 取消登录关闭窗口
+         */
+        private void closeWin(object sender, EventArgs e) {
+            if (System.Windows.Forms.Application.MessageLoop) {
+                var confirmResult = MessageBox.Show("Are you sure to stop login??",
+                                     "Confirm Delete!!",
+                                     MessageBoxButtons.YesNo);
+                if (confirmResult == DialogResult.Yes) {
+                    System.Windows.Forms.Application.Exit();
+                } else {
+                    // If 'No', do something here.
+                }
+            } else {// Console app                
+                System.Environment.Exit(1);
+            }
+        }
+
+        ///**
+        // * 单点登录动画（进度条）
+        // */
+        //private void animate(object sender, EventArgs e) {
+        //    ((ProgressBar)sender).Style = ProgressBarStyle.Marquee;
+        //}
+        /**
+         * 单点登录
+         */
+        private bool sso(int i)  {
+            try {
+                double pow = Math.Pow(i, i);
+                debug(pow);
+                //progressBar1.Invoke(animate, new object[]);
+                //Console.WriteLine(pow);
+                //using (Graphics gr = progressBar1.CreateGraphics()) {
+                //    gr.DrawString(percent + "%",
+                //        SystemFonts.DefaultFont,
+                //        Brushes.Black,
+                //        new PointF(progressBar1.Width / 2 - (gr.MeasureString(percent.ToString() + "%",
+                //            SystemFonts.DefaultFont).Width / 2.0F),
+                //        progressBar1.Height / 2 - (gr.MeasureString(percent.ToString() + "%",
+                //            SystemFonts.DefaultFont).Height / 2.0F)));
+                //}
+
+            } catch (Exception e) {
+                Console.WriteLine("Error Stack {0} ", e.Message);
+            } finally {
+                //System.Threading.Thread.Sleep(5000);
+            }
+            return false;
+        }
+
+        /**
+         * 后台单点登录工作线程
+         */
+        private void workerSSO_DoWork(object sender, DoWorkEventArgs e) {
+            debug("workerSSO_DoWork");
+            //进度条显示
+            sso(6);
+            for (int j = 0; j <= 100; j++) {                
+                workerSSO.ReportProgress(j);
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        private void workerSSO_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+            debug(e.ProgressPercentage);
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            // TODO: do something with final calculation.
+            MessageBox.Show("Done");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        /**
+         *注册右键菜单
+         */
+        private void registerContextMenu() {
+            contextMenu = new ContextMenuStrip();
+            Image icon = Properties.Resources.img_cancel;
+            ToolStripMenuItem menu = new ToolStripMenuItem("Quit", icon, closeWin);
+            contextMenu.Items.Add(menu);
+            this.ContextMenuStrip = contextMenu;
+        }
+
+        /**
+         *双击事件
+         */
+        private void FrmStart_DoubleClick(object sender, EventArgs e) {
+            switch (_lastButtonUp) {
+                case System.Windows.Forms.MouseButtons.Left:
+                    MessageBox.Show("left double click");
+                    break;
+                case System.Windows.Forms.MouseButtons.Right:
+                    MessageBox.Show("right double click");
+                    break;
+                case System.Windows.Forms.MouseButtons.Middle:
+                    MessageBox.Show("middle double click");
+                    break;
+            }
+        }
+
+        MouseButtons _lastButtonUp = MouseButtons.None;
+        private void FrmStart_MouseUp(object sender, MouseEventArgs e) {
+            _lastButtonUp = e.Button;
+        }
+
+        private void debug(object msg) {
+            System.Diagnostics.Debug.Write(DateTime.Now.ToString("[yyyy-MM-dd hh:mm:ss] "));
+            System.Diagnostics.Debug.WriteLine(msg);
+        }
+
+    }
+}
